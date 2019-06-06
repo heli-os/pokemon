26f288ae811f11dca6c133ec7e1244040dd76bd2
Martin Pearson <martin@foretion.com>
2019-06-06 11:01:33 +0900

    add visualstudio gitignore
    add visualstudio gitignore

--- dungreed/nginx_font.h
+++
@@ -1,5 +0,0 @@
-#include "nginx_framework.h"
-
-static ALLEGRO_FONT* default_font;
-void init_font();
-void draw_text(ALLEGRO_COLOR _color, float x, float y, char* msg, int flags);
--- dungreed/nginx_screen.h
+++
@@ -1,5 +0,0 @@
-#include "nginx_framework.h"
-
-extern ALLEGRO_DISPLAY* display;
-
-void create_window(int width, int height, int flags);
